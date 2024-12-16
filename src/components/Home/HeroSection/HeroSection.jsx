1e9e992ee4018987ef90fbb9754109750c47d915
l2a5k8s5 <bhatialakshay960@gmail.com>
2024-12-16 11:25:57 +0530

    Home Page changed

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
                 <div>
                     <small>TOTAL HEALTH CARE SOLUTION</small>
                     <h1>Your Most Trusted <br />Health Partner</h1>
-                    <small>A repudiandae ipsam labore ipsa voluptatum quidem quae laudantium quisquam aperiam maiores sunt fugit, deserunt rem suscipit placeat.</small>
+                    <small>Revolutionizing healthcare through advanced technology and compassionate care, ensuring better patient outcomes.</small>
                 </div>
                 <div className="d-flex justify-content-start gap-2">
                     <Link to={'/doctors'} className="btn-get-started scrollto">Get Started</Link>
