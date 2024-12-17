661668bcbfaba9258d5c077282168e41bcad02ca
l2a5k8s5 <bhatialakshay960@gmail.com>
2024-12-17 17:28:58 +0530

    Hero Section

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import './index.css';
+// import { Link } from 'react-router-dom';
+
+// const HeroSection = () => {
+//     return (
+//         <section id="hero" className="d-flex align-items-center">
+//             <div className="container">
+//                 <div>
+//                     <small>TOTAL HEALTH CARE SOLUTION</small>
+//                     <h1>Your Most Trusted <br />Health Partner</h1>
+//                     <small>Revolutionizing healthcare through advanced technology and compassionate care, ensuring better patient outcomes.</small>
+//                 </div>
+//                 <div className="d-flex justify-content-start gap-2">
+//                     <Link to={'/doctors'} className="btn-get-started scrollto">Get Started</Link>
+//                     <Link to={'/track-appointment'} className="btn-get-started scrollto">Track Appointment</Link>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
+// export default HeroSection;
+
+
 import React from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
@@ -5,18 +29,21 @@ import { Link } from 'react-router-dom';
 const HeroSection = () => {
     return (
         <section id="hero" className="d-flex align-items-center">
-            <div className="container">
-                <div>
-                    <small>TOTAL HEALTH CARE SOLUTION</small>
-                    <h1>Your Most Trusted <br />Health Partner</h1>
-                    <small>Revolutionizing healthcare through advanced technology and compassionate care, ensuring better patient outcomes.</small>
-                </div>
-                <div className="d-flex justify-content-start gap-2">
+            <div className="container d-flex flex-column-reverse flex-md-row align-items-center">
+                <div className="content text-md-start text-center">
+                    <h1>Making Health <br />Care Better Together</h1>
+                    <p>
+                    Health is the foundation of a fulfilling life. It is not just the absence of illness, but a state of complete physical, mental, and emotional well-being. When we prioritize our health, we empower ourselves to live with energy, purpose, and resilience.
+                    </p>
+                    <div className="d-flex justify-content-center justify-content-md-start gap-2">
                     <Link to={'/doctors'} className="btn-get-started scrollto">Get Started</Link>
                     <Link to={'/track-appointment'} className="btn-get-started scrollto">Track Appointment</Link>
+                    </div>
                 </div>
+
             </div>
         </section>
-    )
-}
+    );
+};
+
 export default HeroSection;
