2265d883efbe240159cd9bbffc7504c7d6bdb880
l2a5k8s5 <bhatialakshay960@gmail.com>
2024-12-17 17:44:17 +0530

    Hero Section Change

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import './index.css';
-// import { Link } from 'react-router-dom';
-
-// const HeroSection = () => {
-//     return (
-//         <section id="hero" className="d-flex align-items-center">
-//             <div className="container">
-//                 <div>
-//                     <small>TOTAL HEALTH CARE SOLUTION</small>
-//                     <h1>Your Most Trusted <br />Health Partner</h1>
-//                     <small>Revolutionizing healthcare through advanced technology and compassionate care, ensuring better patient outcomes.</small>
-//                 </div>
-//                 <div className="d-flex justify-content-start gap-2">
-//                     <Link to={'/doctors'} className="btn-get-started scrollto">Get Started</Link>
-//                     <Link to={'/track-appointment'} className="btn-get-started scrollto">Track Appointment</Link>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-// export default HeroSection;
 
 
 import React from 'react';
